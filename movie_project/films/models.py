4eeaeea20b82a7d3aad9fa8d1bdef8c4f32b79e9
Maktub108 <e.kozulin@yandex.ru>
2025-07-01 21:35:01 +0500

    django_forms
    dj_forms_files

---
+++ movie_project/films/models.py
@@ -0,0 +1,11 @@
+from django.db import models
+
+class Film(models.Model):
+    title = models.CharField(max_length=200, verbose_name="Название фильма")
+    description = models.TextField(verbose_name="Описание фильма")
+    review = models.TextField(verbose_name="Отзыв о фильме")
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return self.title
+    
